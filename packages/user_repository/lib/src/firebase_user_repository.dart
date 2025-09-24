d354bdde52a84e4eecc6e9fff71bbfbf05ac6510
sarah elkholy <saraelkholyarmy03@gmail.com>
2025-09-24 15:55:25 +0300

    - created all user methods (signIn-signUp-logOut-ressetPassword)

--- packages/user_repository/lib/src/firebase_user_repository.dart
+++ packages/user_repository/lib/src/firebase_user_repository.dart
@@ -1,3 +1,57 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:user_repository/src/models/my_user.dart';
 import 'user_repo.dart';
 
-class FirebaseUserRepository implements UserRepository {}
+class FirebaseUserRepository implements UserRepository {
+  final FirebaseAuth _firebaseAuth;
+
+  FirebaseUserRepository({required FirebaseAuth? firebaseAuth})
+      : _firebaseAuth = firebaseAuth ?? FirebaseAuth.instance;
+
+  @override
+  Future<MyUser> signUp(MyUser myUser, String password) async {
+    try {
+      UserCredential user = await _firebaseAuth.createUserWithEmailAndPassword(
+          email: myUser.email, password: password);
+
+      myUser = myUser.copyWith(id: user.user!.uid);
+
+      return myUser;
+    } catch (e) {
+      debugPrint(e.toString());
+      rethrow;
+    }
+  }
+
+  @override
+  Future<void> signIn(String email, String password) async {
+    try {
+      await _firebaseAuth.signInWithEmailAndPassword(
+          email: email, password: password);
+    } catch (e) {
+      debugPrint(e.toString());
+      rethrow;
+    }
+  }
+
+  @override
+  Future<void> logOut() async {
+    try {
+      await _firebaseAuth.signOut();
+    } catch (e) {
+      debugPrint(e.toString());
+      rethrow;
+    }
+  }
+
+  @override
+  Future<void> resetPassword(String email) async {
+    try {
+      await _firebaseAuth.sendPasswordResetEmail(email: email);
+    } catch (e) {
+      debugPrint(e.toString());
+      rethrow;
+    }
+  }
+}
